81db86767507dd10f9af6dad7d0c59ae1cbcdde6
harshit973 <harshitahuja2091@gmail.com>
2024-08-05 23:17:13 +0530

    added delete api

--- routes/diet-routes.js
+++ routes/diet-routes.js
@@ -1,45 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const Meal = require("../models/diet.model.js")
+const Meal = require("../models/diet.model.js");
 
-router.post('/addMeal',async(req,res)=>{
-    const meal = new Meal(req.body);
-    await meal.save();
+router.post("/addMeal", async (req, res) => {
+  const meal = new Meal(req.body);
+  await meal.save();
 
+  res.json({
+    success: true,
+    message: "meal data is saved",
+  });
+});
+
+router.patch("/editMeal/:id", async (req, res) => {
+  const mealId = req.params.id;
+  const updatedMeal = req?.body;
+  if (!mealId) {
+    res.status(400).json({
+      message: "path param not available",
+    });
+  } else if (!updatedMeal) {
+    res.status(400).json({
+      message: "payload not available",
+    });
+  } else {
+    await Meal.findOneAndUpdate({ _id: mealId }, { ...updatedMeal });
     res.json({
-        success:true,
-        message:"meal data is saved"
-    })
-
+      success: true,
+      message: "meal data is updated",
+    });
+  }
 });
 
-router.patch('/editMeal/:id',async(req,res)=>{
-    const mealId = req.params.id;
-    const updatedMeal = req?.body;
-    if(!mealId){
-        res.status(400).json({
-            message:"path param not available"
-        })
-    }
-    else if(!updatedMeal){
-        res.status(400).json({
-            message:"payload not available"
-        })
-    }else{
-        await Meal.findOneAndUpdate({_id: mealId},{...updatedMeal});
-        res.json({
-            success:true,
-            message:"meal data is updated"
-        })
-    }
+router.get("/getMeal", async (req, res) => {
+  const allMeal = await Meal.find({}).exec();
+  res.json({
+    success: true,
+    data: allMeal,
+  });
 });
 
-router.get('/getMeal', async(req,res)=>{
-        const allMeal = await Meal.find({}).exec();
-        res.json({
-            success: true,
-            data:allMeal
-        })
-})
+
+router.delete("/:id", async (req, res) => {
+  const mealId = req.params.id;
+  const isDeleted = await Meal.findByIdAndDelete(mealId);
+  if (!isDeleted) {
+    res.json({
+      success: false,
+    });
+  }else{
+    res.json({
+        success: true,
+    });    
+  }
+});
 
 module.exports = router;
